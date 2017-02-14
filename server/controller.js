895e3401c48df62de93424f1d3ba529412fd7b83
laksh95 <goel.laksh95@gmail.com>
2017-02-14 10:34:07 +0530

    basic info form created, connection to db and data inserted into db from the form

--- server/controller.js
+++ server/controller.js
@@ -1,57 +1,14 @@
+
 var fs=require('fs');
 var path=require('path');
-var lodash=require('lodash');
-function readFile(pathname,type,callback)
-{
-	fs.readFile(pathname,type,callback);
-}
-exports.getRepo=function(req,res){
-	var company=req.query.company;
-	var page=req.query.page;
-	var items=[];
-	readFile(path.resolve(__dirname  +"/data.json"),"utf8",readHandler);
-	function readHandler(err,content){
-		var response={
-			sucess:"",
-			data:"",
-			msg:""
-		}
-		if(err){
-			console.log(err);
-			response.sucess=false;
-			response.msg="500 internal error"
-			res.send(JSON.stringify(response));
-		}
-		else
-		{
-			try{
-				var data=JSON.parse(content).items;
-			}catch(exp){
-				response.sucess=false;
-				response.msg="500 internal error";
-				res.send(JSON.stringify(response));
-			}
-			if(data!=null){
-				for(var i=0;i<data.length;i++)
-				{
-					if(lodash.includes(data[i].full_name,company))
-					{	
-						items.push(data[i]);
-					}
-				}
-				var sendItems=[];
-				i=(page-1)*10;
-				for(;i<page*10;i++)
-					sendItems.push(items[i]);
-				response.success=true;
-				response.data=sendItems;
-				res.send(JSON.stringify(response));
-			}
-			else{
-				res.send("null");
-			}
-		}
 
-	}
+var pg=require('pg');
+var connectionString="postgres://postgres:password@localhost:5432/company";
+var client=new pg.Client(connectionString);
+client.connect();
+exports.submitInfo=function(req,res){
+	var query=client.query("insert into users values('"+req.body.fname+"','"+
+		req.body.lname+"','"+req.body.useremail+"','"+req.body.passwd+"');")
+	res.sendFile(path.resolve(__dirname+"/../client/submit.html"));
 
 }
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,12 @@
+
 var controller=require('./controller')
 var path=require('path');
+var bodyParser=require('body-Parser');
 var init=function(app){
+	app.use(bodyParser());
 	app.get('/',function(req,res){
 		res.sendFile(path.resolve(__dirname+"/../client/index.html"));
 	});
-	app.get('/getRepo',controller.getRepo);
+	app.post('/submitInfo',controller.submitInfo);
 }	
 module.exports=init;
